1428a4c3a0ff451a7b42c641cb5fe79c0fb9c5fa
schriker <janekmachine@gmail.com>
2020-11-12 17:39:10 +0100

    Base app skeleton setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from 'pages/home/Home';
+import Search from 'pages/search/Search';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/search/:query">
+          <Search />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/search/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Search = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Unsplash Clone - Search</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default Search;
